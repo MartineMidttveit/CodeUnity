c0dfe1fe5fc9297786b0b7f059e0d71ecc2d26c7
Martine23FT <martinemidttveit@hotmail.com>
2024-01-30 17:48:12 +0100

    Changes

---
+++ hamburger.js
@@ -0,0 +1,33 @@
+const sidebar = document.querySelector(".sidebar");
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+const navLinks = document.querySelectorAll(".navLink");
+const sidebarImage = document.querySelector(".sidebar-image");
+
+hamburgerBtn.addEventListener("click", function () {
+    sidebar.classList.toggle("expanded");
+
+    if (sidebar.classList.contains("expanded")) {
+        sidebar.classList.remove("w-24");
+        sidebar.classList.add("w-60");
+        sidebarImage.style.display = "flex";
+        navLinks.forEach(link => link.style.justifyContent = "flex-start");
+        navLinks.forEach(link => {
+            const span = link.querySelector("span");
+            if (span) {
+                span.style.display = "inline";
+            }
+        });
+
+    } else {
+        sidebar.classList.remove("w-60");
+        sidebar.classList.add("w-24");
+        sidebarImage.style.display = "none";
+        navLinks.forEach(link => link.style.justifyContent = "center");
+        navLinks.forEach(link => {
+            const span = link.querySelector("span");
+            if (span) {
+                span.style.display = "none";
+            }
+        });
+    }
+});
