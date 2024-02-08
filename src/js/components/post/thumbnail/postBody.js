a36159de259ee4edb2a1b294f400cfe09cb305fe
Krihag <125972171+Krihag@users.noreply.github.com>
2024-02-08 22:31:54 +0100

    changes

--- src/js/components/post/thumbnail/postBody.js
+++ src/js/components/post/thumbnail/postBody.js
@@ -1,1 +1,18 @@
-export default function postBody(post, container) {}
+export default function postBody(post, container) {
+  const image = document.createElement("img");
+  image.setAttribute("src", post.media.url);
+  image.setAttribute("alt", "Post Image");
+  image.setAttribute("class", "w-full object-cover rounded-lg my-5");
+
+  const title = document.createElement("h2");
+  title.setAttribute("class", "text-xl font-semibold mb-2");
+  title.textContent = post.title;
+
+  const text = document.createElement("p");
+  text.setAttribute("class", "text-lg");
+  text.textContent = post.body;
+
+  container.append(image, title, text);
+
+  return container;
+}
