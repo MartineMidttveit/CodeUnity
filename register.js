0f42135cb9e9a9b3e9147cace7f3a16a053bd5a2
Martine23FT <martinemidttveit@hotmail.com>
2024-02-12 22:48:15 +0100

    Changes

--- register.js
+++ register.js
@@ -3,7 +3,8 @@ const confirmPasswordInput = document.querySelector("#confirmPassword");
 const passwordRequirements = document.querySelector("#passwordRequirements");
 const passwordLength = document.querySelector("#passwordLengthIcon");
 const passwordFormat = document.querySelector("#passwordFormatIcon");
-const incorrectPasswordMessage = document.querySelector("#incorrectPassword");
+const incorrectPassword = document.querySelector("#incorrectPassword");
+const passwordSymbol = document.getElementById('passwordSymbol');
 
 passwordInput.addEventListener('focus', function () {
     passwordRequirements.style.display = 'flex';
@@ -13,15 +14,17 @@ passwordInput.addEventListener('input', function () {
     const password = this.value;
     const hasValidLength = password.length >= 8;
     const hasValidFormat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/.test(password);
+    const hasSpecialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(password);
 
     passwordLength.style.backgroundColor = hasValidLength ? "#34D399" : "#EF4444";
     passwordFormat.style.backgroundColor = hasValidFormat ? "#34D399" : "#EF4444";
+    passwordSymbol.style.backgroundColor = hasSpecialCharacter ? '#34D399' : '#EF4444';
 });
 
 confirmPasswordInput.addEventListener('input', function () {
     const confirmPassword = this.value;
     const password = passwordInput.value;
-    incorrectPasswordMessage.style.display = password !== confirmPassword ? "block" : "none";
+    incorrectPassword.style.display = password !== confirmPassword ? "block" : "none";
 });
 
 confirmPasswordInput.addEventListener('blur', function () {
@@ -30,7 +33,7 @@ confirmPasswordInput.addEventListener('blur', function () {
 
     if (password === '' && confirmPassword === "") {
         setTimeout(function () {
-            incorrectPasswordMessage.style.display = "none";
+            incorrectPassword.style.display = "none";
         }, 1000);
     }
 });
