2d83b566e162c5ac33166d1a820f55ec020bf990
Martine23FT <martinemidttveit@hotmail.com>
2024-02-12 15:38:55 +0100

    Changes

--- src/js/components/post/comps/detailsWithOptions.js
+++ src/js/components/post/comps/detailsWithOptions.js
@@ -26,5 +26,55 @@ export default function userOptions(post) {
   options.appendChild(optionsDiv);
   container.appendChild(details);
   container.appendChild(options);
+
+  button.addEventListener("click", function () {
+    toggleOptions();
+    optionsDiv.style.cssText =
+      "min-width: 300px; right: 0; overflow: hidden; padding: 1.5rem;";
+  });
+
+  function toggleOptions() {
+    optionsDiv.classList.toggle("hidden");
+
+    if (!optionsDiv.classList.contains("hidden")) {
+      optionsDiv.innerHTML = "";
+      const option1 = document.createElement("button");
+      option1.classList.add("block");
+
+      const icon1 = document.createElement("i");
+      icon1.classList.add(
+        "fa-regular",
+        "fa-pen-to-square",
+        "text-secondary",
+        "text-xl",
+        "mr-5"
+      );
+      option1.textContent = "Edit post";
+      option1.prepend(icon1);
+
+      const separateLine = document.createElement("div");
+      separateLine.classList.add("border-t", "bg-primary", "muted", "my-5");
+      separateLine.style.height = "1px";
+
+      const option2 = document.createElement("button");
+      option2.classList.add("block");
+
+      const icon2 = document.createElement("i");
+      icon2.classList.add(
+        "fa-solid",
+        "fa-trash",
+        "text-secondary",
+        "text-xl",
+        "mr-5"
+      );
+      option2.textContent = "Delete post";
+      option2.prepend(icon2);
+
+      optionsDiv.appendChild(option1);
+      optionsDiv.appendChild(separateLine);
+      optionsDiv.appendChild(option2);
+    }
+  }
+
   return container;
 }
