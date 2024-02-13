72b3a881abc5fef1035c55fa1d4382edce4a9c58
Krihag <125972171+Krihag@users.noreply.github.com>
2024-02-13 20:45:31 +0100

    changes

--- src/js/components/modal/elements/input.js
+++ src/js/components/modal/elements/input.js
@@ -1,8 +1,12 @@
 export default function input() {
   const container = document.createElement("div");
   const label = document.createElement("label");
+  label.classList.add("text-lg");
   const input = document.createElement("input");
-
+  input.setAttribute(
+    "class",
+    "w-full border h-14 rounded mb-4 mt-2 outline-none pl-3"
+  );
   container.append(label, input);
   return container;
 }
