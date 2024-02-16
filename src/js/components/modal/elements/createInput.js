77c26195edd6706b972d04d5cddfc9b7d9390941
Krihag <125972171+Krihag@users.noreply.github.com>
2024-02-16 13:19:03 +0100

    changes

--- src/js/components/modal/elements/createInput.js
+++ src/js/components/modal/elements/createInput.js
@@ -1,17 +1,26 @@
 export default function input(ele) {
   const input = document.createElement("input");
-  input.classList.add("w-full", "border", "h-14", "rounded", "mb-4", "mt-2", "outline-none", "pl-3");
+  input.classList.add(
+    "w-full",
+    "border",
+    "h-14",
+    "rounded",
+    "mb-4",
+    "mt-2",
+    "outline-none",
+    "pl-3"
+  );
 
   if (ele.text) {
     const label = document.createElement("label");
     label.classList.add("text-lg");
     label.textContent = ele.text;
-    input.setAttribute("aria-label", ele.text); 
-    input.id = ele.text.toLowerCase().replace(/\s+/g, '-'); 
-    label.setAttribute("for", input.id); 
-    input.setAttribute("placeholder", ele.placeholder || ''); 
-    input.value = ele.value || ''; 
-    return [label, input]; 
+    input.setAttribute("aria-label", ele.text);
+    input.id = ele.text.toLowerCase().replace(/\s+/g, "-");
+    label.setAttribute("for", input.id);
+    input.setAttribute("placeholder", ele.placeholder || "");
+    input.value = ele.value || "";
+    // return [label, input];
   } else {
     if (ele.placeholder) {
       input.setAttribute("placeholder", ele.placeholder);
@@ -19,6 +28,6 @@ export default function input(ele) {
     if (ele.value) {
       input.value = ele.value;
     }
-    return input; 
   }
+  return input;
 }
