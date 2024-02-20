0ba2e844a621ebd22f497f6deb522b3b864c6510
Martine23FT <martinemidttveit@hotmail.com>
2024-02-20 14:31:15 +0100

    Changes

--- src/js/components/post/comps/tags.js
+++ src/js/components/post/comps/tags.js
@@ -3,9 +3,10 @@ export default function tags(tags) {
   container.setAttribute("class", "pt-4 gap-x-5 flex flex-wrap");
 
   tags.forEach((tag) => {
+    const hashtag = tag.includes("#") ? tag : `#${tag}`;
     const span = document.createElement("span");
     span.setAttribute("class", "text-secondary 2xl:text-lg");
-    span.textContent = `#${tag}`;
+    span.textContent = hashtag;
     container.appendChild(span);
   });
   return container;
