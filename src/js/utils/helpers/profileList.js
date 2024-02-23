e205a81560984ad6af2ee5e4f39260f0507eeda4
Krihag <125972171+Krihag@users.noreply.github.com>
2024-02-23 04:15:28 +0100

    changes

--- src/js/utils/helpers/profileList.js
+++ src/js/utils/helpers/profileList.js
@@ -5,12 +5,14 @@ export default function profileList(profiles, user, container, number = 5) {
     let profile = profiles[randomNum];
     if (!numbersArray.includes(randomNum)) {
       if (user.name !== profile.name) {
-        if (!profile.followers.includes(user.name)) {
-          if (
-            profile.avatar.url !==
-            "https://images.unsplash.com/photo-1579547945413-497e1b99dac0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&q=80&h=400&w=400"
-          )
-            numbersArray.push(randomNum);
+        if (
+          !profile.followers.some((follower) => follower.name === user.name)
+        ) {
+          //   if (
+          //     profile.avatar.url !==
+          //     "https://images.unsplash.com/photo-1579547945413-497e1b99dac0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&q=80&h=400&w=400"
+          //   )
+          numbersArray.push(randomNum);
         }
       }
     }
